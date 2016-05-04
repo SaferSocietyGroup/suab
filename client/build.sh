f8dec7360b68b4187d183ae84d9cde2103bd4e09
Erik Larkö <erik@larko.se>
2016-05-05 01:39:06 +0200

    Fixed unset GOPATH in build script

--- client/build.sh
+++ client/build.sh
@@ -5,13 +5,18 @@ set -e
 mkdir -p build
 BUILD_CMD="go build -v suab"
 
-echo "## Compling for linux..."
-GOOS=linux $BUILD_CMD
-mv -f suab build/
-echo "## Done"
+DIRNAME=`dirname $0`
+WD=`readlink -nf $DIRNAME`
+export GOPATH=$WD
 
+function build {
+  echo "## Compling for $1..."
+  GOOS=$1 $BUILD_CMD
+  mv -f suab* build/
+  echo "## Done"
+}
+
+
+build linux
 echo
-echo "## Compiling for Windows"
-GOOS=windows $BUILD_CMD
-mv -f suab.exe build/
-echo "## Done"
+build windows
