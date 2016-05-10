45841e32e73364e6673fdc1f63b6ffd2aee7bcd1
Erik Larkö <erik@larko.se>
2016-05-10 22:21:13 +0200

    Commented the clone.sh script and showed that it can take environment variables

--- client/clone.sh
+++ client/clone.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-git clone --depth=1 https://github.com/SaferSocietyGroup/suab.git .
+## This script will checkout the git repository at $REPO_URL and put it in
+## $CHECKOUT_DESTINATION. Note that $GIT_COMMIT is expected to passed in as
+## an environment variable. This is achieved by the --env flag to suab. If
+## omitted, it will checkout master by default
+
+REPO_URL="https://github.com/SaferSocietyGroup/suab.git"
+DEFAULT_REVISION="master"
+CHECKOUT_DESTINATION="."
+
+set -x
+git clone \
+	--depth=1 \
+	--branch=${GIT_COMMIT:-$DEFAULT_REVISION} \
+	$REPO_URL \
+	$CHECKOUT_DESTINATION
