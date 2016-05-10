a2851a38ba40abd55f38749b5b24aa5147ff9c16
Erik Larkö <erik@larko.se>
2016-05-10 19:11:09 +0200

    Added a build script and the dependencies to the server

---
+++ server/build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+set -e
+
+BUILD_CMD="go build -v suab-server"
+
+READPATH=`readlink -nf $0`
+WD=`dirname $READPATH`
+export GOPATH=$WD
+mkdir -p "${WD}/build"
+
+function build {
+  echo "## Compling for $1..."
+  GOOS=$1 $BUILD_CMD
+  mv -f suab* "${WD}/build"
+  echo "## Done"
+}
+
+build linux
+echo
+build windows
+
