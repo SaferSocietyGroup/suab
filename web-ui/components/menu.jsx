50da70a9ca3cb45958f7a990416c1b0da50f280a
Erik Larkö <erik@larko.se>
2016-06-16 21:01:00 +0200

    Refactored the menu

--- web-ui/components/menu.jsx
+++ web-ui/components/menu.jsx
@@ -4,7 +4,9 @@ import { Link } from "react-router"
 const linuxUrl = server + "/client/linux"
 const winUrl = server + "/client/win"
 
-const styles = {
+const menuContainerStyle = {display: "flex", flexDirection: "row", flexWrap: "nowrap", justifyContent: "space-between"};
+
+const downloadContainerStyle = {
     float: "right",
     margin: "10px",
     border: "1px solid black",
@@ -12,21 +14,26 @@ const styles = {
     paddingBottom: "10px",
     height: "100%",
 };
+const downloadLinkStyle = {marginLeft: "20px"};
+
+const headerContainerStyle ={float: "left", margin: downloadContainerStyle.margin, color: "black"};
+const headerStyle = {fontFamily: "jura", margin: "0px"};
+const tagLineStyle = {fontFamily: "lavanderia", fontSize: "2.5em", margin: "0px"};
 
 export default function(props) {
 
-    return <div style={{display: "flex", flexDirection: "row", flexWrap: "nowrap", justifyContent: "space-between"}}>
+    return <div style={menuContainerStyle}>
             <Link to="/">
-                <div style={{float: "left", margin: styles.margin, color: "black"}}>
-                    <h1 style={{fontFamily: "jura", margin: "0px"}}>SUAB</h1>
-                    <h2 style={{fontFamily: "lavanderia", fontSize: "2.5em", margin: "0px"}}> - we put a shell in your build</h2>
+                <div style={headerContainerStyle}>
+                    <h1 style={headerStyle}>SUAB</h1>
+                    <h2 style={tagLineStyle}> - we put a shell in your build</h2>
                 </div>
             </Link>
             <div></div>
-            <fieldset style={styles}>
+            <fieldset style={downloadContainerStyle}>
                 <legend>Download client</legend>
-                <a href={linuxUrl} style={{marginLeft: "20px"}}><img src='images/linux.png' /></a> &nbsp;
-                <a href={winUrl} style={{marginLeft: "20px"}}><img src='images/windows.png' /></a>
+                <a href={linuxUrl} style={downloadLinkStyle}><img src='images/linux.png' /></a> &nbsp;
+                <a href={winUrl} style={downloadLinkStyle}><img src='images/windows.png' /></a>
             </fieldset>
         </div>
 }
